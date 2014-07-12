0dc250936695112a5e788e0355f4e78f63c7a510
Nadav Spiegelman <me@nadav.name>
2014-07-12 00:13:21 -0400

    Portfolio experiments

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,3 +4,14 @@
 */
 $(".fitvids").fitVids();
 $.bigfoot();
+
+$(".portfolio__toggle").click(function(e) {
+  e.preventDefault();
+  var container = $(this).next(".portfolio__preview");
+  var iframe = container.find(".portfolio__preview__iframe");
+  container.toggleClass("portfolio__preview--visible");
+
+  if (container.hasClass("portfolio__preview--visible")) {
+    iframe.attr("src", iframe.data("src"));
+  }
+});
